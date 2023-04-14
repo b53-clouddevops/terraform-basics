215a850d60d63fd8f4101feec772fca5a7ce770d
CodingManoj <mvp2612@hotmail.com>
2023-04-13 22:58:22 -0400

    Git Auto Push

--- 03-example/ec2.tf
+++ 03-example/ec2.tf
@@ -26,7 +26,7 @@ resource "aws_security_group" "allow_ssh_sg" {
   ingress {
     description      = "SSH from VPC"
     from_port        = 22
-    to_port          = 2
+    to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
