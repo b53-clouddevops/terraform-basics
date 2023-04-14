9751a5bbe5a4eedf2c073ef85df5598c16222616
CodingManoj <mvp2612@hotmail.com>
2023-04-13 23:02:57 -0400

    Git Auto Push

--- 03-example/ec2.tf
+++ 03-example/ec2.tf
@@ -5,8 +5,9 @@ provider "aws" {
 
 # Block to create EC2
 resource "aws_instance" "web" {
-  ami           = "ami-02fd2e8a76f43c89e"
-  instance_type = "t3.nano"
+  ami                       = "ami-02fd2e8a76f43c89e"
+  instance_type             = "t3.nano"
+  vpc_security_group_ids    =  aws_security_group.allow_ssh_sg.id
 
   tags = {
     Name        = "Terraform-Instance-Server-Name"
@@ -17,6 +18,7 @@ resource "aws_instance" "web" {
 output "private_dns_of_server" {
     value       = aws_instance.web.private_dns
 }
+
 
 # Creates Security Group 
 resource "aws_security_group" "allow_ssh_sg" {
