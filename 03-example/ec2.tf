884010caef4a681eae0a7f9a3f779fbac3843f9c
CodingManoj <mvp2612@hotmail.com>
2023-04-13 22:56:07 -0400

    Git Auto Push

--- 03-example/ec2.tf
+++ 03-example/ec2.tf
@@ -9,11 +9,37 @@ resource "aws_instance" "web" {
   instance_type = "t3.nano"
 
   tags = {
-    Name = "Terraform-Instance-Server-Name"
+    Name        = "Terraform-Instance-Server-Name"
   }
 }
 
 # Block to print the attribute 
 output "private_dns_of_server" {
-    value = aws_instance.web.private_dns 
+    value       = aws_instance.web.private_dns
+}
+
+# Creates Security Group 
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 2
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
 }
