82d9d350bafebb7fe485231264219c6c6592adf8
CodingManoj <mvp2612@hotmail.com>
2023-04-13 22:38:34 -0400

    Git Auto Push

--- 03-example/ec2.tf
+++ 03-example/ec2.tf
@@ -1,9 +1,9 @@
 # Always start with declaring provider information first 
-
 provider "aws" {
    region = "us-east-1"
 }
 
+# Block to create EC2
 resource "aws_instance" "web" {
   ami           = "ami-02fd2e8a76f43c89e"
   instance_type = "t3.micro"
@@ -11,4 +11,9 @@ resource "aws_instance" "web" {
   tags = {
     Name = "Terraform-Instance"
   }
+}
+
+# Block to print the attribute 
+output "private_dns_of_server" {
+    value = aws_instance.web.private_dns 
 }
