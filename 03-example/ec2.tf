6d0bc5a08c554a0bc7822df175cdfe10dda39251
CodingManoj <mvp2612@hotmail.com>
2023-04-13 22:44:39 -0400

    Git Auto Push

--- 03-example/ec2.tf
+++ 03-example/ec2.tf
@@ -6,7 +6,7 @@ provider "aws" {
 # Block to create EC2
 resource "aws_instance" "web" {
   ami           = "ami-02fd2e8a76f43c89e"
-  instance_type = "t3.micro"
+  instance_type = "t3.nano"
 
   tags = {
     Name = "Terraform-Instance-Server-Name"
