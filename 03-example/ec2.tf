01aa3b1d6312a271d3263e84a36f1e4f6cc9c6f1
CodingManoj <mvp2612@hotmail.com>
2023-04-13 23:06:44 -0400

    Git Auto Push

--- 03-example/ec2.tf
+++ 03-example/ec2.tf
@@ -1,8 +1,3 @@
-# Always start with declaring provider information first 
-provider "aws" {
-   region = "us-east-1"
-}
-
 # Block to create EC2
 resource "aws_instance" "web" {
   ami                       = "ami-02fd2e8a76f43c89e"
@@ -17,30 +12,4 @@ resource "aws_instance" "web" {
 # Block to print the attribute 
 output "private_dns_of_server" {
     value       = aws_instance.web.private_dns
-}
-
-
-# Creates Security Group 
-resource "aws_security_group" "allow_ssh_sg" {
-  name        = "b53_allow_ssh_sg"
-  description = "Allow SSH inbound traffic"
-
-  ingress {
-    description      = "SSH from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "b53_allow_ssh_sg"
-  }
 }
