24490e9871d7a8672bd789dbed182df303bb1733
CodingManoj <mvp2612@hotmail.com>
2023-04-14 22:32:47 -0400

    Git Auto Push

--- 04-examples/ec2/main.tf
+++ 04-examples/ec2/main.tf
@@ -2,14 +2,14 @@
 resource "aws_instance" "web" {
   ami                       = "ami-02fd2e8a76f43c89e"
   instance_type             = "t3.nano"
-  vpc_security_group_ids    = [var.sg]
+  vpc_security_group_ids    = [var.sg_id]
 
   tags = {
     Name        = "Terraform-Instance-Server-Name"
   }
 }
 
-variable "sg" {}
+variable "sg_id" {}
 
 # output "private_dns" {
 #     value = aws_instance.web.private_dns
