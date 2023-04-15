b1ebe90ca290c1708d788411050d519f6c623d81
CodingManoj <mvp2612@hotmail.com>
2023-04-14 22:31:29 -0400

    Git Auto Push

--- 04-examples/sg/main.tf
+++ 04-examples/sg/main.tf
@@ -24,7 +24,6 @@ resource "aws_security_group" "allow_ssh_sg" {
 }
 
 # Declarre the ID of the SG ID, so that we can pass it to root module 
-
-output "sg" {
+output "sg_id" {
     value   =   "aws_security_group.allow_ssh_sg.id"
 }
