5a794f580f9f84f31903fbba0b081ebb96bbc28c
CodingManoj <mvp2612@hotmail.com>
2023-04-16 22:27:49 -0400

    Git Auto Push

--- 04-examples/ec2/data.tf
+++ 04-examples/ec2/data.tf
@@ -1,5 +1,5 @@
 data "aws_ami" "my_ami" {
   most_recent      = true
-  name_regex       = "Centos7-Linux-Learning-AMI"
+  name_regex       = "Lab-Ami-With-Ansible_Installed"
   owners           = ["self"] 
 }
